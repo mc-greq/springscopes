900d21e71288da2ffd064329aea18768c9392a45
mc-greq <greq19@gmail.com>
2019-01-02 14:40:34 +0100

    Scope singleton test

--- src/example/nio/TrackCoach.java
+++ src/example/nio/TrackCoach.java
@@ -21,4 +21,14 @@ public class TrackCoach implements Coach {
     public String getDailyFortune() {
         return "Just Do It: " + fortuneService.getFortune();
     }
+
+    // add an init method
+    public void doMysStartupStuff(){
+        System.out.println("TrackCoach: inside method doMysStartupStuff");
+    }
+
+    // add a destroy method
+    public void doMyCleanupStuff(){
+        System.out.println("TrackCoach: inside method doMyCleanupStuff");
+    }
 }
