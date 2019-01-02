1ac66781b51deea49e698b77590bd06db5f3faba
mc-greq <greq19@gmail.com>
2019-01-02 14:15:27 +0100

    Scope prototype test

--- src/example/nio/Main.java
+++ src/example/nio/Main.java
@@ -1,8 +1,30 @@
 package example.nio;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // load the spring configuration file
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // retrieve the bean from spring container
+
+        Coach theCoach = context.getBean("myCoach", Coach.class);
+        Coach alphaCoach = context.getBean("myCoach", Coach.class);
+
+        // check if these are the same
+        boolean result = (theCoach == alphaCoach);
+        System.out.println("\nPointing to the same object " + result);
+
+        System.out.println("\nMemory location for theCoach: " + theCoach);
+
+        System.out.println("\nMemory location for theCoach: " + alphaCoach + "\n");
+
+        //close the context
+        context.close();
+
     }
 }
